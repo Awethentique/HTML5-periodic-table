d5f0310133e71577fe56acd584d7014ca2c076d7
Ritesh Makan <ritesh.makan@gmail.com>
2023-01-24 01:25:10 +0100

    Update content and styling

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ESLintPlugin = require('eslint-webpack-plugin');
 const PrettierPlugin = require('prettier-webpack-plugin');
@@ -20,7 +21,25 @@ const configurePrettier = () => {
   return new PrettierPlugin();
 }
 
+// configure Resolve
+const configureResolveAlias = () => {
+  return {
+    alias: {
+      'assets': path.resolve(__dirname, '../src/images')
+    }
+  }
+}
+
+// Configure HtmlWebpackInlineSVGPlugin
+const configureHtmlWebpackInlineSVGPlugin = () => {
+  return new HtmlWebpackInlineSVGPlugin({
+    runPreEmit: true,
+    inlineAll: true,
+  });
+}
+
 module.exports = {
+  resolve: configureResolveAlias(),
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.pug",
@@ -29,20 +48,25 @@ module.exports = {
     new MiniCssExtractPlugin(),
     configureEsLint(),
     configurePrettier(),
+    configureHtmlWebpackInlineSVGPlugin(),
   ],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.pug$/,
-        use: [
-          {
+        use: [{
             loader: "pug-loader",
+        }, ],
           },
-        ],
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
       },
     ],
   },
