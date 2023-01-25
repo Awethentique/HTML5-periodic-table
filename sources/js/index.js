f92ced2aa353fce80e3aa284130bc6eb74fda018
Ritesh Makan <ritesh.makan@gmail.com>
2023-01-25 01:45:26 +0100

    Handle phone landscape mode

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,4 +1,13 @@
 import '../scss/index.scss';
 $(document).ready(function () {
   console.log('js-file');
+  $('.menu').on('click', function () {
+    $('.key').toggleClass('hidden');
+    $('.menu').toggleClass('open');
+  });
+  $('.key label').on('click', function (e) {
+    e.stopPropagation();
+    $('.key').addClass('hidden');
+    $('.menu').removeClass('open');
+  });
 });
